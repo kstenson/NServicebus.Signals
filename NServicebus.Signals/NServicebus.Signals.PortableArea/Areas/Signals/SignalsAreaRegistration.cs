26e9976a62d94b8a02de1e94a5b3f3cea29d7530
Keith Stenson <kstenson@gmail.com>
2012-08-10 16:44:47 +0100

    First commit

---
+++ NServicebus.Signals/NServicebus.Signals.PortableArea/Areas/Signals/SignalsAreaRegistration.cs
@@ -0,0 +1,54 @@
+﻿using System.Web.Mvc;
+using MvcContrib.PortableAreas;
+
+namespace NServicebus.Signals.PortableArea.Areas.Signals
+{
+    public class SignalsAreaRegistration : PortableAreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Signals";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context, IApplicationBus bus)
+        {
+            RegisterRoutes(context);
+            RegisterAreaEmbeddedResources();
+        }
+
+        private void RegisterRoutes(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                AreaName + "_scripts",
+                base.AreaRoutePrefix + "/Scripts/{resourceName}",
+                new { controller = "EmbeddedResource", action = "Index", resourcePath = "scripts" },
+                new[] { "NServicebus.Signals.PortableArea" }
+            );
+
+            context.MapRoute(
+                AreaName + "_images",
+                base.AreaRoutePrefix + "/images/{resourceName}",
+                new { controller = "EmbeddedResource", action = "Index", resourcePath = "images" },
+                new[] { "NServicebus.Signals.PortableArea" }
+            );
+
+            context.MapRoute(
+                AreaName + "_default",
+                base.AreaRoutePrefix + "/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "NServicebus.Signals.PortableArea.Areas.Signals.Controllers", "MvcContrib" }
+            );
+        }
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Signals_default",
+                "Signals/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ NServicebus.Signals/NServicebus.Signals.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NServicebus.Signals.Web.MvcApplication" Language="C#" %>
